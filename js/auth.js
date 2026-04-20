739550f120348769318a5b66d48eb389b067816b
super777apps <super777apps@gmail.com>
2026-04-20 16:18:02 +1000

    Add files via upload

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,8 @@ import { auth, db } from "./firebase.js";
 
 import {
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+signInWithEmailAndPassword,
+sendPasswordResetEmail
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 import {
